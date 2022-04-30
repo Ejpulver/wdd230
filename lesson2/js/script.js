896f694437ea43ccecfd8348555c460768cc85a9
Ejpulver <pul15004@byui.edu>
2022-04-30 16:37:16 -0600

    made some general changes

---
+++ lesson2/js/script.js
@@ -0,0 +1,36 @@
+const days = [
+    "Sunday",
+    "Monday", 
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+const date = new Date(document.lastModified);
+const dayName = days[date.getDay()];
+const monthName = months[date.getMonth()];
+const year = date.getFullYear();
+const fullDate = `${dayName}, ${date.getDate()} ${monthName} ${year}`;
+
+document.getElementById("lastmodified").textContent = `Last modified: ${fullDate}`;
+
+let copyrightYear = new Date().getFullYear();
+
+document.querySelector(".copyright").textContent = `\u00A9 ${copyrightYear} | Easton Pulver | Utah`
