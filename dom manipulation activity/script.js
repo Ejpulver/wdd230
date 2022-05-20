ad2665a7c2bbf5ec758baf28ff26213103c6abfa
Ejpulver <pul15004@byui.edu>
2022-05-20 00:02:29 -0600

    lots of things

---
+++ dom manipulation activity/script.js
@@ -0,0 +1,26 @@
+const input = document.getElementById('favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('.list');
+
+button.addEventListener('click', () => {
+    const myItem = input.value;
+    input.value = '';
+
+    const listItem = document.createElement("li");
+    const list_button = document.createElement("button");
+    const list_text = document.createElement('span');
+
+    listItem.append(list_text);
+    list_text.append(myItem);
+    listItem.append(list_button);
+    list_button.textContent = '❌';
+    list.append(listItem);
+
+    list_button.addEventListener('click', () => {
+        list.remove(listItem);
+    })
+
+    input.focus();
+}); 
+
+
