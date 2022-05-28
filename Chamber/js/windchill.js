5cf57314655332107027742bb4b8f246d1f0fdde
Ejpulver <pul15004@byui.edu>
2022-05-28 15:02:09 -0600

    changes to windchill file

--- Chamber/js/windchill.js
+++ Chamber/js/windchill.js
@@ -1,12 +1,8 @@
-let wind_speed = 5;
-let temperature = 39;
-let wind_chill;
+const wind_speed = parseInt(document.querySelector(".wind-speed").innerHTML);
+const temperature = parseInt(document.querySelector(".temperature").innerHTML);
+const windchill = document.querySelector(".wind-chill");
 
-wind_chill = 35.74 + (0.6215 * temperature) - 35.75 * (wind_speed ** 0.16) + (0.4275 * temperature) * (wind_speed ** 0.16);
-
-if (temperature <= 50 && wind_speed > 3) {
-document.getElementsByClassName(".wind-chill").textContent = `Wind Chill: ${wind_chill} F`
-}
-else {
-    document.getElementsByClassName(".wind-chill").textContent = `Wind Chill: N/A`
+if ((temperature <= 50) && (wind_speed < 3)) {
+    const f = 35.74 + (0.6215 * temperature) - (35.775 * Math.pow(wind_speed, 0.16)) + (0.4275 * temperature * Math.pow(wind_speed, 0.16));
+    windchill.textContent = f.toFixed(1) + `°F`;
 }
