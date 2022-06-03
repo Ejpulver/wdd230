74f0bb999c005d9962399c8ae3f1e1097a109e3c
Ejpulver <pul15004@byui.edu>
2022-06-02 19:15:33 -0600

    lazy loading assignment

--- Chamber/js/windchill.js
+++ Chamber/js/windchill.js
@@ -2,7 +2,7 @@ const wind_speed = parseInt(document.querySelector(".wind-speed").innerHTML);
 const temperature = parseInt(document.querySelector(".temperature").innerHTML);
 const windchill = document.querySelector(".wind-chill");
 
-if ((temperature <= 50) && (wind_speed < 3)) {
+if ((temperature <= 50) && (wind_speed > 3)) {
     const f = 35.74 + (0.6215 * temperature) - (35.775 * Math.pow(wind_speed, 0.16)) + (0.4275 * temperature * Math.pow(wind_speed, 0.16));
     windchill.textContent = f.toFixed(1) + `°F`;
 }
