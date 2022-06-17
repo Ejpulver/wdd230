49d740c40aba6a5a9b5d5b150bca512dcd767f62
Ejpulver <pul15004@byui.edu>
2022-06-16 20:36:09 -0600

    finished JSON activity plus added it to homepage

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -1,7 +1,15 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 const cards = document.querySelector('.cards');
-
-const prophets = jsonObject['prophets'];
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.table(data);  // temporary checking for valid response and data parsing
+    const prophets = data['prophets']
+    prophets.forEach(displayProphets);
+  });
+  
 
 function displayProphets(prophet) {
   let card = document.createElement('section');
@@ -16,9 +24,9 @@ function displayProphets(prophet) {
   POB.textContent = 'Place of Birth:' + ' ' + prophet.birthplace;
 
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-  ____.setAttribute('src', prophet.imageurl);
-  ____.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
-  ____.setAttribute('loading', 'lazy');
+  portrait.setAttribute('src', prophet.imageurl);
+  portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
+  portrait.setAttribute('loading', 'lazy');
 
   // Add/append the section(card) with the h2 element
   card.appendChild(h2);
@@ -30,13 +38,3 @@ function displayProphets(prophet) {
   // Add/append the existing HTML div with the cards class with the section(card)
   document.querySelector('div.cards').appendChild(card);
 }
-
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    prophets.forEach(displayProphets);
-  });
- 
