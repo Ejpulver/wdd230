8b65ab59c5083591035d29b9610317982ac74ff3
Ejpulver <pul15004@byui.edu>
2022-06-24 17:36:55 -0700

    finished activity

--- Chamber/weather-api.js
+++ Chamber/weather-api.js
@@ -2,15 +2,17 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = '{https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=888875481bdd43d17422e63acf4cbc50}'
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=888875481bdd43d17422e63acf4cbc50';
 
-async function apiFetch() {
+apiFetch(url);
+
+async function apiFetch(apiURL) {
     try {
-      const response = await fetch(url);
+      const response = await fetch(apiURL);
       if (response.ok) {
         const data = await response.json();
         console.log(data); // this is for testing the call
-        // displayResults(data);
+        displayResults(data);
       } else {
           throw Error(await response.text());
       }
@@ -18,5 +20,14 @@ async function apiFetch() {
         console.log(error);
     }
   }
+
+  function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
   
-  apiFetch();
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = toTitleCase(weatherData.weather[0].description);
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
