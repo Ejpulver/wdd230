42feef1bdda3e13e72ff5e5573263163bbb0f450
Ejpulver <pul15004@byui.edu>
2022-06-24 18:34:56 -0700

    added dynamically driven weather

--- Chamber/js/script.js
+++ Chamber/js/script.js
@@ -49,3 +49,28 @@ if (dayName == `Monday` || dayName == `Tuesday`) {
 
 document.querySelector(".wind-speed").textContent = `Wind Speed: 5 mph`
 
+const temperature = document.querySelector(".temperature-2");
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Provo&units=imperial&appid=888875481bdd43d17422e63acf4cbc50';
+
+apiFetch(url);
+
+async function apiFetch(apiURL) {
+    try {
+        const response = await fetch(apiURL);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }
+        else {
+            throw Error(await response.text()); 
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(weatherData) {
+    temperature.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+}
--- Chamber/js/windchill.js
+++ Chamber/js/windchill.js
@@ -4,5 +4,8 @@ const windchill = document.querySelector(".wind-chill");
 
 if ((temperature <= 50) && (wind_speed > 3)) {
     const f = 35.74 + (0.6215 * temperature) - (35.775 * Math.pow(wind_speed, 0.16)) + (0.4275 * temperature * Math.pow(wind_speed, 0.16));
-    windchill.textContent = f.toFixed(1) + `°F`;
+    windchill.innerHTML = f.toFixed(1) + `°F`;
+}
+else {
+    windchill.innerHTML = `N/A`;
 }
